d0c05888b2cb602b90693634747f710cf4651d6a
Serafim <Nikiforov.ov@gmail.com>
2026-04-14 23:22:44 +0500

    v7 :(

--- database.py
+++ database.py
@@ -8,5 +8,9 @@ from sqlalchemy.ext.declarative import declarative_base
 DATABASE_URL = "sqlite+aiosqlite:///./app.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
 Base = declarative_base()
