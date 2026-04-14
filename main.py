acd14fb038c0771e15b0da6f9d3654e1411b4692
Serafim <Nikiforov.ov@gmail.com>
2026-04-14 22:32:30 +0500

    fix: update code for SQLAlchemy 2.0 and Pydantic v2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlalchemy.future import select
 
 import models
 import schemas
-from database import engine, session
+from database import engine, async_session
 
 app = FastAPI()
 
@@ -18,39 +18,39 @@ async def startup():
 
 @app.on_event("shutdown")
 async def shutdown():
-    await session.close()
     await engine.dispose()
 
 
-@app.post("/recipes/", response_model=schemas.RecipeIn)
-async def add_recipe(recipe: schemas.RecipeIn) -> models.Recipe:
-    new_recipe = models.Recipe(**recipe.dict())
-    async with session.begin():
+@app.post('/recipes/', response_model=schemas.RecipeIn)
+async def recipe(recipe: schemas.RecipeIn) -> models.Recipe:
+    async with async_session() as session:
+        new_recipe = models.Recipe(**recipe.model_dump())
         session.add(new_recipe)
         await session.commit()
-    return new_recipe
+        await session.refresh(new_recipe)
+        return new_recipe
 
 
-@app.get("/recipes/", response_model=List[schemas.RecipeList])
+@app.get('/recipes/', response_model=List[schemas.RecipeList])
 async def recipes() -> List[models.Recipe]:
-    res = await session.execute(
-        select(models.Recipe).order_by(
-            models.Recipe.views.desc(), models.Recipe.time.desc()
+    async with async_session() as session:
+        res = await session.execute(
+            select(models.Recipe).order_by(
+                models.Recipe.views.desc(),
+                models.Recipe.time.desc()
+            )
         )
-    )
-    return res.scalars().all()
+        return res.scalars().all()
 
 
-@app.get("/recipes/{recipe_id}", response_model=schemas.RecipeDetail)
-async def check_recipes(
-    recipe_id: int,
-):
-    res = await session.execute(
-        select(models.Recipe).where(models.Recipe.id == recipe_id)
-    )
-    recipe_real = res.scalar_one_or_none()
-    if not recipe_real:
-        raise HTTPException(status_code=404, detail="Рецепт не найден")
-    recipe_real.views += 1
-    await session.commit()
-    return recipe_real
+@app.get('/recipes/{recipe_id}', response_model=schemas.RecipeDetail)
+async def check_recipes(recipe_id: int):
+    async with async_session() as session:
+        res = await session.execute(select(models.Recipe).where(models.Recipe.id == recipe_id))
+        recipe_real = res.scalar_one_or_none()
+        if not recipe_real:
+            raise HTTPException(status_code=404, detail="Рецепт не найден")
+        recipe_real.views += 1
+        await session.commit()
+        await session.refresh(recipe_real)
+        return recipe_real
